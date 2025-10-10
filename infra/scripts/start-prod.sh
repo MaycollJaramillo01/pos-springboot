8c8a5c49fca19a9018f3c53f608f9ddeaf5203ea
RaveHunter05 <paulsotelor@yahoo.es>
2025-10-10 16:27:00 -0600

    fix: infra for uploading spring boot app to droplet

---
+++ infra/scripts/start-prod.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export ENVIRONMENT=production
+export SPRING_PROFILE=production
+export SPRING_DATASOURCE_URL=jdbc:postgresql://postgres:5432/mydatabase
+
+docker-compose -f docker-compose.prod.yml up -d
+echo "Todos los servicios corriendo en producción"
